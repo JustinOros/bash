ff6ee95815cd693e48bc650ee6557c94e58b4149
Justin Oros <JustinOros@gmail.com>
2023-07-09 19:31:26 -0700

    Added support for middle name
    Added support for more than FirstName LastName (e.g. John William Smith)

--- uapay.sh
+++ uapay.sh
@@ -5,63 +5,59 @@
 # Usage: uapay.sh FirstName LastName
 #################################################
 
-# Check to see if BASH is installed
-{
+# BASH present?
 if [ ! -f /bin/bash ]; then
     echo "Bash not found!"
     exit 0
 fi
-}
 
-# Check to see if CURL is installed
-{
+# CURL present?
 if [ ! -f /usr/bin/curl ]; then
     echo "Curl not found!"
     exit 0
 fi
-}
 
-# Check to see if GREP is intalled
-{
+# GREP present?
 if [ ! -f /usr/bin/grep ]; then
     echo "Grep not found!"
     exit 0
 fi
-}
 
-# URL for UA Salaries
+# UA Payroll URL
 uapay_url='https://openpayrolls.com/employee'
 
 # User Agent
 uapay_useragent='Chrome/79'
 
-# Get Input from User
-{
+# Get Input
 if [ -z "$1" ] || [ -z "$2" ]; then
     echo -e "\nUsage: uapay.sh FirstName LastName\n"
     exit 0
 else
-    uapay_name=$1-$2
+    for i in "$@"
+    do
+        uapay_name="$@"
+    done
 fi
-}
+
+# Format Input
+uapay_formatted_name=${uapay_name// /-}-4329
 
 # Grep Filter
 uapay_filter="and a pay of"
 
 # Query URL
-uapay_output=$(curl -s -k --user-agent $uapay_useragent $uapay_url/$uapay_name-4329 | grep "$uapay_filter")
+uapay_query=$(curl -s -k --user-agent $uapay_useragent $uapay_url/$uapay_formatted_name | grep "$uapay_filter")
 
-# Check output
-{
-if [ -z "$uapay_output" ]; then
-    echo -e "\n$1 $2 was not found.\n"
+# Check Results
+if [ -z "$uapay_query" ]; then
+    echo -e "\n$uapay_name was not found.\n"
     exit 0
 fi
-}
 
-# Format output
-uapay_output=${uapay_output/\<p class=\"lead font-16 font-weight-400\"\>/}
-uapay_output=${uapay_output/\<\/p\>/}
+# Format Output
+uapay_query=${uapay_query/\<p class=\"lead font-16 font-weight-400\"\>/}
+uapay_query=${uapay_query/\<\/p\>/}
 
-# Print output
-echo -e "\n$uapay_output\n"
+# Print Output
+echo -e "\n$uapay_query\n"
